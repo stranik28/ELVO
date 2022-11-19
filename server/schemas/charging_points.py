279914cae4a7bab9d1d5000ed2db6d8fb6462043
stranik28 <jb357506@gmail.com>
2022-11-19 03:19:51 +0300

    change schemas

--- server/schemas/charging_points.py
+++ server/schemas/charging_points.py
@@ -4,6 +4,7 @@ from typing import List
 class ChargingPoint(BaseModel):
     address: str # Address of charging
     types_of_charging: List[int] # Тип коннкетора
+    coordinates: str # Coordinates of charging
     number_of_places: int # КОличество зарядных мест
     available_count: int # Количество доступных зарядных мест
     cost: int # Стоимость зарядки
